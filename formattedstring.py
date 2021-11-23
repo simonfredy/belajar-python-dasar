3ac38c550e6fb1f678d0d07c67310aab8cbc8dcc
simonfredy <simonfredysinaga@gmail.com>
2021-11-23 08:08:03 +0700

    Formatted String

---
+++ formattedstring.py
@@ -0,0 +1,14 @@
+first_name = "Simon"
+last_name = "Sinaga"
+
+message = first_name + " [" + last_name + "]"
+message = f"{first_name} [{last_name}]"
+
+print(message)
+print(message)
+
+
+
+age = 31
+message_second = f"Umur kamu {age}"
+print(message_second)
